abfa39b6d7a1d854ed4f77067e6be8d923ef6ff8
Mearaj <mearajbhagad@gmail.com>
2023-01-11 23:11:41 +0530

    added thumbnail shown when app link shared

--- frontend/config_common.go
+++ frontend/config_common.go
@@ -4,7 +4,7 @@ const ApiURLKey = "API_URL"
 const StaticFolderKey = "STATIC_FOLDER"
 const InnerPortKey = "INNER_PORT"
 const DefaultApiURL = "http://localhost:8001"
-const DefaultStaticFolder = "/dist"
+const DefaultStaticFolder = "dist"
 const DefaultInnerPort = "8080"
 
 type Config struct {
